1ad278896ac43734697f6039a6da367a8e8676c0
libuse.poustkova <libuse.poustkova@skoda.cz>
2021-03-29 17:18:14 +0200

    panacek beha a zarazi se na okraji

--- main.js
+++ main.js
@@ -10,38 +10,51 @@ if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || pan
 // po spusteni yacne hrat hudba
 // onkeydown podminka sipky nahoru, dolu...
 
-let center_left = window.innerWidth;
-let center_up = window.innerHeight;
+let panacek, panacekX, panacekY, panacekW, panacekH, panacekPozice;
+let mince, minceX, minceY, minceW, minceH;
 
 
-
+function priNacteni() {
+    panacek = document.getElementById('panacek');
+    mince = document.getElementById('mince');
+    panacekW = panacek.width;
+    panacekH = panacek.height;
+    panacekPozice = umisti_panacka(window.innerWidth/2, window.innerHeight/2);
+    minceW = mince.width
+    minceH = mince.height
+    minceX, minceY = umisti_minci()
+    console.log(panacekPozice)
+}
 
 function stiskKlavesy(udalost) {
 
     //console.log(udalost.key);
 
+     hraj('#hudba');
 
-    hraj('#hudba');
-    // umis
-    umisti_panacka(window.innerWidth/2, window.innerHeight/2)
-    umisti_minci()
 
     if (udalost.key == 'ArrowUp') {
-     umisti_panacka(window.innerWidth/2, window.innerHeight/2)
-     console.log(udalost.key);
-
+     panacek.src = "obrazky/panacek-nahoru.png";
+     panacekPozice = umisti_panacka( panacekPozice[0], panacekPozice[1] - 10);
+     console.log(udalost.key,  panacekPozice[0], panacekPozice[1]);
     	}
 
     else if (udalost.key == 'ArrowDown') {
+    panacek.src = "obrazky/panacek.png";
+    panacekPozice = umisti_panacka( panacekPozice[0], panacekPozice[1] + 10);
     console.log(udalost.key);
     // jsi ještě děťátko
     }
     else if (udalost.key == 'ArrowLeft') {
+    panacek.src = "obrazky/panacek-vlevo.png";
+    panacekPozice = umisti_panacka( panacekPozice[0] - 10, panacekPozice[1]);
     console.log(udalost.key);
     // důchodce nebo cokoliv jiného
     }
 
     else if (udalost.key == 'ArrowRight'){
+    panacek.src = "obrazky/panacek-vpravo.png";
+    panacekPozice = umisti_panacka( panacekPozice[0] + 10, panacekPozice[1]);
     console.log(udalost.key);
     // důchodce nebo cokoliv jiného
     }
@@ -49,16 +62,29 @@ function stiskKlavesy(udalost) {
 }
 
 function umisti_minci() {
-    let poz_l = document.getElementById('mince').style.left = Math.floor(Math.random() * window.innerWidth)  + "px";
-    let poz_t = document.getElementById('mince').style.top =  Math.floor(Math.random() * window.innerHeight) + "px";
+    let poz_l = mince.style.left = Math.floor(Math.random() * window.innerWidth)  + "px";
+    let poz_t = mince.style.top =  Math.floor(Math.random() * window.innerHeight) + "px";
     console.log(poz_l, poz_t);
     return poz_l, poz_t;
 }
 
 function umisti_panacka(x,y) {
-    let poz_l = document.getElementById('panacek').style.left = x + "px";
-    let poz_t = document.getElementById('panacek').style.top = y + "px";
-    return poz_l, poz_t;
+    if (x < 0) {
+    x = 0;
+    }
+    if (y < 0) {
+        y = 0;
+        }
+    if (x > window.innerWidth-panacekW) {
+        x = window.innerWidth-panacekW ;
+    }
+    if (y > window.innerHeight-panacekH) {
+            y = window.innerHeight-panacekH;
+        }
+    let poz_l = panacek.style.left = x + "px";
+    let poz_t = panacek.style.top = y + "px";
+
+    return [parseInt(poz_l), parseInt(poz_t)];
 }
 
 function hraj(elementSelector) {
